d145beb531b0004037781862ff6671658bc2d218
Evgeniy <Xorsiphus@yandex.ru>
2021-07-23 01:20:21 +0700

    warning fix

--- WorkScheduleManagement.Application/CQRS/Queries/GetUserCountOfUnusedVacationDaysPastYears.cs
+++ WorkScheduleManagement.Application/CQRS/Queries/GetUserCountOfUnusedVacationDaysPastYears.cs
@@ -12,42 +12,41 @@ namespace WorkScheduleManagement.Application.CQRS.Queries
     public class GetUserCountOfUnusedVacationDaysPastYears
     {
         public record Query(string Id) : IRequest<int>;
-        
-                public class Handler : IRequestHandler<Query, int>
+
+        public class Handler : IRequestHandler<Query, int>
+        {
+            private readonly AppDbContext _context;
+
+            public Handler(AppDbContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<int> Handle(Query request, CancellationToken cancellationToken)
+            {
+                int totalPastYearsVacationDays = 0;
+
+                for (var i = 2021; i < DateTime.Today.Year; i++)
                 {
-                    private readonly AppDbContext _context;
-        
-                    public Handler(AppDbContext context)
-                    {
-                        _context = context;
-                    }
-        
-                    public async Task<int> Handle(Query request, CancellationToken cancellationToken)
-                    {
-                        DateTime firstDay = new DateTime(2021, 1, 1);
+                    var startD = new DateTime(i, 1, 1);
+                    var endD = new DateTime(i, 12, 31);
 
-                        int totalPastYearsVacationDays = 0;
+                    var userVacationRequests = await _context.VacationRequests
+                        .Where(r => r.DateFrom > startD &&
+                                    r.DateTo < endD &&
+                                    r.Creator.Id == request.Id &&
+                                    r.RequestStatus.Id == RequestStatus.Approved)
+                        .ToListAsync();
+                    var yearVacationDays = userVacationRequests.Aggregate(0,
+                        (cur, vacationRequest) =>
+                            cur + Convert.ToInt32(vacationRequest.DateTo.Subtract(vacationRequest.DateFrom).TotalDays) +
+                            1);
 
-                        for (var i = 2021; i < DateTime.Today.Year; i++)
-                        {
-                            var startD = new DateTime(i, 1, 1);
-                            var endD = new DateTime(i, 12, 31);
-                            
-                            var yearVacationDays = _context.VacationRequests
-                                .Where(r => r.DateFrom > startD &&
-                                            r.DateTo < endD && 
-                                            r.Creator.Id == request.Id &&
-                                            r.RequestStatus.Id == RequestStatus.Approved)
-                                .ToListAsync().Result
-                                .Aggregate(0,
-                                    (cur, vacationRequest) =>
-                                        cur + Convert.ToInt32(vacationRequest.DateTo.Subtract(vacationRequest.DateFrom).TotalDays) + 1);
-
-                            totalPastYearsVacationDays += yearVacationDays;
-                        }
-
-                        return totalPastYearsVacationDays;
-                    }
+                    totalPastYearsVacationDays += yearVacationDays;
                 }
+
+                return totalPastYearsVacationDays;
+            }
+        }
     }
 }
